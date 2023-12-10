b96121c1c4d9f81e5042f35f48022304a799cee6
rimjhimart <rimjhim.gupta.ak@gmail.com>
2023-12-11 02:13:36 +0530

    practice

--- new2.js
+++ new2.js
@@ -1,43 +1,9 @@
-<<<<<<< HEAD
-// Practice #2
-console.log("Practicing JavaScript Part-2");
-console.log("\n");
-console.log("Primitive Datatype & Non-Primitive Datatype");
+// change the card title to red
+let ctitle = document.getElementsByClassName("card-title")[0]
+ctitle.style.color = "red"
 
-// Primitive datatype
-//Total 7 type of primitive data type
-//nn ss bb u "n-number,n-NULL,b-BigInt,b-boolean,s-Symbol,S-string,u-Undefined"
-console.log("\n");
-
-a=null;
-b=56;
-c="alok";
-d=true;
-e=Symbol("javScript");
-f=BigInt(290);
-g= undefined;
-
-console.log(a,b,c,d,e,f,g);
-
-//for knowing the which data type it is for this we have to go for "consol.log(type of VAR)"
-//eg:-
-console.log(typeof e); //output:- Symbol
-
-// NON-Primitive datatype
-//In javascript the non-primitive data type is known as OBJECTS
-const item = {
-    "Alok" : 45
-}
-console.log(item["Alok"]); //output:- 45
-
-//for this if there is not a defined VARIABLE in object then the output generated will be :undefined
-
-console.log(item["ahah"]); //output:- undefined
-=======
-let t = document.body.firstElementChild.firstElementChild
-console.log(t)
-console.log(t.rows)
-console.log(t.caption)
-console.log(t.tHead.firstElementChild)
-console.log(t.tBodies)
->>>>>>> cf60768 (practice)
+let ctitles = document.querySelectorAll(". card-title")
+ctitles[0].style.color = "green"
+ctitles[1].style.color = "yellow"
+ctitles[2].style.color = "blue"
+console.log(ctitles)
