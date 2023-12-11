4c1d42c95cf19119117b215d8790a62457d310ff
rimjhimart <rimjhim.gupta.ak@gmail.com>
2023-12-12 00:20:18 +0530

    practice set

---
+++ a.js
@@ -0,0 +1,3 @@
+document.getElementsByTagName("nav")[0].firstElementChild.style.color = "red"
+document.getElementsByTagName("nav")[0].firstElementChild.style.color = "green"
+document.getElementsByTagName("nav")[0].lastElementChild.style.color = "green"
