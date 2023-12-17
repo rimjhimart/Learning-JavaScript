7f762584ff3492dc11599978c41b381294be5bee
rimjhimart <rimjhim.gupta.ak@gmail.com>
2023-12-17 23:55:23 +0530

    events practice

---
+++ pr9.js
@@ -0,0 +1,7 @@
+//browser events
+
+let a= document.getElementsByClassName("container")[0]
+a.onclick = ()=>{
+    let b= document.getElementsByClassName("container")[0]
+    b.innerHTML = "Hello world"
+}
