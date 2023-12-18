9964282ece649f207499e7ad9adf53a3462e99c6
rimjhimart <rimjhim.gupta.ak@gmail.com>
2023-12-19 05:06:20 +0530

    finally clause practice

---
+++ a6.js
@@ -0,0 +1,21 @@
+
+const f =()=>{
+
+           try {
+    let a= 0
+    //console.log(program)
+    console.log("Program run succesfully")
+    return
+           } catch (error) {
+    console.log("this is an error")
+           }
+           finally{
+               console.log("i am a good girl")
+               //close the file
+               //exit the loop
+               //write to the log file
+               //usually we do final clean ups
+           }
+}
+f()
+console.log("end")
