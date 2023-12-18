c6eb772d74bc8ba190b074688acd73c306529736
rimjhimart <rimjhim.gupta.ak@gmail.com>
2023-12-19 04:21:22 +0530

    practice

---
+++ a4.js
@@ -0,0 +1,21 @@
+setTimeout(()=>{
+    console.log("hacking wifi....please wait...")
+}, 1000)
+
+try{
+    console.log(keshav)  // keshav is not defined so this will show an error
+}
+catch(error){          // to overcome this error we use try and catch which helps in way that if it find any error it will allow to rest code to run
+    console.log("balle balle")
+}
+
+
+setTimeout(()=>{
+    console.log("fetching username and password....please wait...")
+}, 2000)
+setTimeout(()=>{
+    console.log("hacking keshav's facebook i'd....please wait...")
+}, 3000)
+setTimeout(()=>{
+    console.log("keshav's id and password fetched!")
+}, 4000) 
