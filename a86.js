f99f00f4a743deab78143a34b91aaf07686ac094
rimjhimart <rimjhim.gupta.ak@gmail.com>
2023-12-28 23:45:08 +0530

    Practice

---
+++ a86.js
@@ -0,0 +1,18 @@
+//IIFE
+let a = ()=>{  new Promise((resolve, reject) => {
+    setTimeout(()=>{
+        resolve(456)
+    }, 4000)
+});
+}
+
+
+(async()=>{
+let b = await a()
+console.log(b)
+let c= await a()
+console.log(c)
+let d = await a()
+console.log(d)
+}) ()
+
