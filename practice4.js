4c8db983f03993262f674447c76d08afad2812eb
rimjhimart <rimjhim.gupta.ak@gmail.com>
2024-01-03 23:38:56 +0530

    practice

---
+++ practice4.js
@@ -0,0 +1,24 @@
+//string methods
+
+let name = 'alokr'
+
+//turns the txt into uppercase
+console.log(name.toUpperCase())
+
+//turns the txt into lowercase
+console.log(name.toLowerCase())
+
+//will print only the letter counts from 2 to 4
+console.log(name.slice(3))
+
+//replace the previous txt and u can add what u want instead of that
+console.log(name.replace("r","rimu"))
+
+// concat helps to add two string within the sentences
+let name1 = "harry"
+let name2 = "naman"
+console.log(name1.concat(" is a friend of ",name2, " as he said "))
+
+//trim used to remove the whitespaces in the selected string
+let name3 = "    rimmu  "
+console.log(name3.trim())
