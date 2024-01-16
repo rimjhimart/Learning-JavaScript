6c5115d7fa1fd7f521679908485ac8ffa09692ae
rimjhimart <rimjhim.gupta.ak@gmail.com>
2024-01-16 23:19:16 +0530

    practice

---
+++ save3.js
@@ -0,0 +1,12 @@
+//example for else if
+let x = 10;
+
+if (x > 5) {
+  console.log("x is greater than 5");
+} else {
+  console.log("x is not greater than 5");
+}
+// example for for loop
+for (let i = 0; i < 5; i++) {
+    console.log("Iteration " + (i + 1));
+  }
