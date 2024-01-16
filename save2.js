576b99d383e4678d4c35ff232984c97968369332
rimjhimart <rimjhim.gupta.ak@gmail.com>
2024-01-16 23:18:59 +0530

    practice

---
+++ save2.js
@@ -0,0 +1,11 @@
+//example for for in loop
+let myObject = {
+    name: "John",
+    age: 25,
+    city: "New York"
+  };
+  
+  for (let key in myObject) {
+    console.log(key + ": " + myObject[key]);
+  }
+
