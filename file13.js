82ce693440a6ca236eaca2779b519541fc4416ca
rimjhimart <rimjhim.gupta.ak@gmail.com>
2024-02-29 23:49:27 +0530

    canvas

---
+++ file13.js
@@ -0,0 +1,9 @@
+// file13.js
+document.addEventListener('DOMContentLoaded', function () {
+    const canvas = document.getElementById('myCanvas');
+    const context = canvas.getContext('2d');
+  
+    // Your drawing operations go here
+    context.fillStyle = 'blue';
+    context.fillRect(50, 50, 100, 100);
+  });
