2e49ef0bf19acd20d9598e36ca0e3f534182343a
rimjhimart <rimjhim.gupta.ak@gmail.com>
2024-04-12 23:56:19 +0530

    practice

---
+++ file22.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var button = document.getElementById("clickMeButton");
+    button.addEventListener("click", function() {
+        alert("You clicked the button!");
+    });
+});
